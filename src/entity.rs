486bbb7a6972c5f5635dd21fbdd5132eda80d930
Erick Daniel Corona <edcoronag@gmail.com>
2020-07-05 19:31:05 -0500

    Add Display impl for Entity, use in main.rs

--- src/entity.rs
+++ src/entity.rs
@@ -1,6 +1,22 @@
-mod entity {
+pub mod entity {
+    use std::string;
+    use std::fmt;
+
     pub struct Entity {
-        name: str,
+        id: u32,
+        name: string::String,
         color: u32,
+    }
+
+    impl Entity {
+        pub fn create(id: u32, name: string::String, color: u32) -> Entity {
+            Entity { id, name, color }
+        }
+    }
+
+    impl fmt::Display for Entity {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "(ID: {}, Name: {}, Color: {:X})", self.id, self.name, self.color)
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use entity;
+mod entity;
+pub use entity::entity::Entity;
 
 fn main() {
     let id = 0;
-    let name = "Perro";
+    let name = "something".to_string();
     let color = 0xFF0B10;
 
-    let something = Entity(id, name, color);
+    let something = Entity::create(id, name, color);
     println!("Shunyata: influence simulation");
     println!("==============================");
+
+    println!("Entity: {}", something);
 }
