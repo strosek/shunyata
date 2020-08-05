eee3044ebad22c8280c09df81ae3b65473b7cb5c
Erick Corona <erick.corona@openmarket.com>
2020-08-05 18:33:04 -0500

    Generate groups of interactions

--- src/universe.rs
+++ src/universe.rs
@@ -1,6 +1,5 @@
 pub mod universe {
     use crate::entity::entity::Entity;
-    use bitvec::prelude::*;
     use rand::Rng;
     use serde::{Deserialize, Serialize};
     use std::string;
@@ -11,7 +10,6 @@ pub mod universe {
         id: string::String,
         n_beings: usize,
         state: vec::Vec<Entity>,
-        next_state: vec::Vec<Entity>,
     }
 
     /* Methods:
@@ -25,13 +23,11 @@ pub mod universe {
     impl Universe {
         pub fn new(id: string::String, n_beings: usize) -> Universe {
             let state = vec::Vec::<Entity>::new();
-            let next_state = vec::Vec::<Entity>::new();
 
             Universe {
                 id,
                 n_beings,
                 state,
-                next_state,
             }
         }
 
@@ -55,48 +51,58 @@ pub mod universe {
                     influence,
                     value as f64,
                 ));
-
-                self.next_state.push(Entity::new(
-                    i as u32,
-                    "entity".to_string(),
-                    plasticity,
-                    influence,
-                    value as f64,
-                ));
             }
 
-            self.tick();
+            // TODO: change this for a loop statement.
+            for _ in 0..100 {
+                self.tick();
+            }
         }
 
         fn tick(&mut self) {
-            for _ in 0..self.state.len() {
-                self.evaluate_interaction(&self.get_who());
+            let interactions = &self.get_random_interactions();
+            for i in 0..interactions.len() {
+                self.evaluate_interaction(&interactions[i]);
             }
         }
 
-        fn get_who(&self) -> Vec<usize> {
-            /* Get who meets who, according to spacial collisions, randomly or whatever algorithm
-             * is implemented.
-             */
+        fn get_random_interactions(&self) -> Vec<Vec<usize>> {
+            /// Get who interacts with who, randomly.
+
             let mut rng = rand::thread_rng();
-            let mut interacted = bitvec![0; self.n_beings];
-            interacted.set_all(false);
-
-            let n_meets = rng.gen_range(2, self.n_beings / 5);
-
-            let mut meets = vec::Vec::<usize>::new();
-            for _ in 0..n_meets {
-                meets.push(rng.gen_range(0, self.n_beings));
+            let mut interacted = Vec::<bool>::with_capacity(self.n_beings);
+            for _ in 0..self.n_beings {
+                interacted.push(false);
             }
 
-            meets
+            let n_groups = rng.gen_range(2, self.n_beings / 3);
+            let mut groups = Vec::<Vec<usize>>::new();
+
+            for _ in 0..n_groups {
+                let n_encounters = rng.gen_range(2, self.n_beings / 5);
+
+                let mut encounter = vec::Vec::<usize>::new();
+
+                let mut i_encounters = 0usize;
+                while i_encounters < n_encounters {
+                    let entity_index = rng.gen_range(0, self.n_beings);
+                    if ! interacted[entity_index] {
+                        encounter.push(entity_index);
+                        i_encounters += 1;
+                        interacted[entity_index] = true;
+                    }
+                }
+                groups.push(encounter);
+            }
+
+            groups
         }
 
-        fn evaluate_interaction(&mut self, meets: &vec::Vec<usize>) {
+        fn evaluate_interaction(&mut self, encounter: &vec::Vec<usize>) {
             // Calculate average of entities that met.
             let mut sum = 0.0f64;
             let mut i = 0usize;
-            for meet in meets {
+            for meet in encounter {
                 let entity = &self.state[*meet];
                 sum += entity.value;
                 print!(" Color: {:.2}", entity.value);
@@ -106,7 +112,7 @@ pub mod universe {
 
             // Calculate target value according to each entity's influence factor.
             let mut target = average;
-            for meet in meets {
+            for meet in encounter {
                 let entity = &self.state[*meet];
 
                 // Make target more similar to entities with their influence values.
@@ -119,14 +125,14 @@ pub mod universe {
             }
 
             // Make entities similar to target.
-            for meet in meets {
+            for meet in encounter {
                 let entity = &self.state[*meet];
                 // Make target more similar to entities with their influence values.
                 if entity.value < target {
-                    self.next_state[*meet].value += (target - entity.value) / 2.0f64;
+                    self.state[*meet].value += (target - entity.value) / 2.0f64;
                 }
                 else if entity.value > target {
-                    self.next_state[*meet].value -= (entity.value- target) / 2.0f64;
+                    self.state[*meet].value -= (entity.value- target) / 2.0f64;
                 }
             }
             print!("\n");
