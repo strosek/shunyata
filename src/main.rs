02299abfcef930a9b3c72b0233c948a85f475ac7
Erick Daniel Corona <edcoronag@gmail.com>
2020-11-05 17:06:56 -0600

    Increase number of entities

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,18 @@ use std::string;
 pub use universe::universe::Universe;
 
 fn main() {
-    //println!("Shunyata: influence simulation");
-    //println!("==============================");
+    println!("Shunyata: influence simulation");
+    println!("==============================");
 
     let mut universe = Universe::new(
         string::String::from("somewhere"),
         1_000_000usize,
-        20usize,
+        100usize,
         3,
-        666.0,
     );
     universe.spawn("resources/universe_cfg.json");
 
     //let ser_universe = serde_json::to_string(&universe).unwrap();
     //println!("{}", ser_universe);
+    println!("All done!");
 }
